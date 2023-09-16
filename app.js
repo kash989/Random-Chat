748c6c367807723f93773bd86a32e07a117cb3d9
kash989 <jkashish576@gmail.com>
2023-09-16 16:15:14 +0530

    mobile changes

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ socket.in('room'+ arg[1]).emit('recieveMsg',arg[0]);
  
 });
 
-const port= process.env.port || 3000;
+const port= process.env.PORT || 3000;
 httpServer.listen(port, "0.0.0.0", function()
 {
   console.log('server running');
